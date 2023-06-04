abf85bdbb72c04136f6378e3c7b6aa3a8bf260a2
Wolfhill09 <harvey.akhurst@icloud.com>
2023-06-04 22:43:11 +0100

    Tweak: fxmanifest

--- [Weapons]/fxmanifest.lua
+++ [Weapons]/fxmanifest.lua
@@ -1,12 +1,7 @@
 fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
-shared_scripts {
-	"cfg/*.lua",
-}
-
 files {
-	"data/loadouts.meta",
 	'**/weaponcomponents.meta',
     '**/weaponarchetypes.meta',
 	'**/weaponanimations.meta',
@@ -17,8 +12,7 @@ files {
 data_file 'WEAPONCOMPONENTSINFO_FILE' '**/weaponcomponents.meta'
 data_file 'WEAPON_METADATA_FILE' '**/weaponarchetypes.meta'
 data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
-data_file "LOADOUTS_FILE" "data/loadouts.meta"
 data_file 'PED_PERSONALITY_FILE' '**/pedpersonality.meta'
 data_file 'WEAPONINFO_FILE' '**/weapons.meta'
 
-client_script 'cl_weaponNames.lua'
+client_script 'weaponNames.lua'
